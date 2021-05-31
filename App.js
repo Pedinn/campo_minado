c4352b10398c8f1079db018eb051454e58594532
Pedro <pedrosilonliveira05@gmail.com>
2021-05-31 16:24:05 -0300

    enter

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import params from './src/params'
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Iniciando o Mines!!</Text>
-        <Text style={styles.instructions}>tamanho da grade: 
-        {params.getRowAmount()}x{params.getColumnsAmount()} </Text>
-        <StatusBar style="auto" />
-      </View>
-    );
-  }
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>ola</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -22,10 +17,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  Welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
   },
 });
