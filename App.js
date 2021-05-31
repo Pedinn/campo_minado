ed8e86baf000c6025fa297697fa4fd67e6aa61bb
Pedro <pedrosilonliveira05@gmail.com>
2021-05-31 16:21:25 -0300

    enter

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { render } from 'react-dom';
 import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params'
 
 export default class App extends Component {
   render() {
