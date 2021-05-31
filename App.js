c8926d46dfc51cbff1844e4e9b72e745b00aec5c
Pedro <pedrosilonliveira05@gmail.com>
2021-05-31 16:39:55 -0300

    enter

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params'
 import Field from './src/components/Field'
 
+
 export default function App() {
   return (
     <View style={styles.container}>
