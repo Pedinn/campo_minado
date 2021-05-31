3997e40b6a4e36c2744421a3c32fb7eef3874fe0
Pedro <pedrosilonliveira05@gmail.com>
2021-05-31 16:30:50 -0300

    enter

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params'
+import Field from './src/components/Field'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>ola</Text>
+      <Text style={styles.welcome}>Iniciando o Mines!!</Text>
+      <Text style={styles.instructions}>Tamanho da grade: 
+      {params.getRowAmount()}x{params.getColumnsAmount()} </Text>
       <StatusBar style="auto" />
+
+      <Field />
+      <Field opened />
+      <Field opened nearMines={1}/>
+      <Field opened nearMines={2}/>
+      <Field opened nearMines={3}/>
+      <Field opened nearMines={6}/>
+      <Field mined />
+      <Field mined opened />
+      <Field mined opened exploded />
+      <Field flagged />
+      <Field flagged opened/>
+
     </View>
   );
 }
@@ -17,5 +34,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
 });
